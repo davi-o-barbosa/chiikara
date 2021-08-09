a4adfaaae0e0417c39f2aed0d803bd7eca2b9a02
Davi Oliveira <davicicfaculdade@gmail.com>
2021-08-09 14:00:44 -0300

    Comando de buscar animes finalizado Talvez ainda precise de um clean-up

--- src/helpers/anilist.ts
+++ src/helpers/anilist.ts
@@ -1,21 +1,20 @@
 import { MessageEmbed } from 'discord.js';
 import fetch, { Response } from 'node-fetch';
 
-const status: { [key: string]: string } = {
+const statusDict: { [key: string]: string } = {
 	'FINISHED': 'Finalizado',
-	'RELEASING': 'Lançando',
+	'RELEASING': 'Sendo transmitido',
 	'NOT_YET_RELEASED': 'Em breve',
 	'CANCELLED': 'Cancelado',
 	'HIATUS': 'Hiato',
 };
 
-const season: { [key: string]: string } = {
+const seasonDict: { [key: string]: string } = {
 	'WINTER': 'Inverno',
 	'SPRING': 'Primavera',
 	'SUMMER': 'Verão',
 	'FALL': 'Outono',
 };
-
 
 async function fetchAnime(anime: string): Promise<any> {
 	const url = 'https://graphql.anilist.co';
@@ -35,10 +34,28 @@ async function fetchAnime(anime: string): Promise<any> {
 	return json.data.Media;
 }
 
+function getStreaming(data: any): string {
+	const streamWebsites = ['Crunchyroll', 'Netflix', 'Funimation', 'Hidive', 'Hulu'];
+	let output = '\n**Streaming:** ';
+
+	for (const site of data) {
+		if (streamWebsites.includes(site.site)) {
+			output += `[${site.site}](${site.url}), `;
+		}
+	}
+
+	return output.substring(0, output.length - 2);
+}
+
 export async function getAnime(anime: string): Promise<MessageEmbed | null> {
 	const data = await fetchAnime(anime);
-
 	if (!data) return null;
+
+	const season = data.season ? seasonDict[data.season] : 'N/A';
+	const seasonYear = data.seasonYear ?? 'N/A';
+	const meanScore = data.meanScore ? `${data.meanScore / 10}/10` : 'N/A';
+	const episodes = data.episodes ? `${data.episodes}` : 'N/A';
+	const streaming = data.isLicensed ? getStreaming(data.externalLinks) : 'Nenhum streaming disponível =(';
 
 	const embed = new MessageEmbed()
 		.setTitle(data.title.romaji)
@@ -47,9 +64,22 @@ export async function getAnime(anime: string): Promise<MessageEmbed | null> {
 		.setURL(data.siteUrl)
 		.setImage(data.bannerImage)
 		.setFooter('Dados disponibilizados pelo AniList e trace.moe.', 'https://anilist.co/img/icons/android-chrome-512x512.png')
-		.addField('Temporada', `${season[data.season]} - ${data.seasonYear}`, true)
-		.addField('Status', status[data.status], true)
-		.addField('Nota - Anilist', `${data.meanScore / 10}/10`, true);
+		.addField('Temporada', `${season} - ${seasonYear}`, true)
+		.addField('Avaliação', meanScore, true)
+		.addField('Episódios:', episodes, true)
+		.addField('Status', statusDict[data.status] + streaming, true);
+
+
+	if (data.status === 'RELEASING') {
+		if (!data.nextAiringEpisode.airingAt) return embed;
+
+		const episode = data.nextAiringEpisode.episode;
+		const date = new Date(data.nextAiringEpisode.airingAt * 1000);
+		const string = date.toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+
+		embed.addField('Próximo episódio: ', `**Episódio:** ${episode}\n**Horário:** ${string}`, true);
+	}
+
 	return embed;
 }
 
