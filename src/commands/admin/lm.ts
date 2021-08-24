6c49eda36abe1326a03eac28cfc9c3b09a32f7dc
Davi Oliveira <davicicfaculdade@gmail.com>
2021-08-24 13:25:50 -0300

    lm command done.

--- src/commands/admin/lm.ts
+++ src/commands/admin/lm.ts
@@ -1,6 +1,6 @@
-import { CommandInteraction, GuildMember, MessageEmbed } from 'discord.js';
+import { CommandInteraction, GuildMember, MessageEmbed, TextChannel, Message } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { LastMessage, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 export default {
 	data: new SlashCommandBuilder()
@@ -12,11 +12,12 @@ export default {
 				.setDescription('Usuário no qual você deseja ver a última mensagem.')
 				.setRequired(false),
 		),
+
 	async execute(interaction: CommandInteraction, prisma: PrismaClient): Promise<void> {
 		let member = interaction.options.getMember('membro') as GuildMember | null;
 		member = member ?? interaction.member as GuildMember;
 
-		const user = await prisma.lastMessage.findUnique({
+		const lMessage = await prisma.lastMessage.findUnique({
 			where: {
 				userId_guildId: {
 					userId: member.user.id,
@@ -25,20 +26,37 @@ export default {
 			},
 		});
 
-		if (!user) {
+		if (!lMessage) {
 			return await interaction.reply({
-				content: 'Esse usuário não possui uma mensagem nos meus registros.',
+				content: 'Esse usuário não possui uma mensagem salva nos meus registros.',
 				ephemeral: true,
 			});
 		}
 
-		return await interaction.reply({
-			embeds: [generateEmbed(user)],
-		});
+		const channel = await interaction.guild?.channels.fetch(lMessage.channelId) as TextChannel | null;
 
+		if (!channel) return await interaction.reply({ content: 'O canal dessa mensagem não está mais disponível.', ephemeral: true });
+
+		try {
+			const fetchedMessage = await channel.messages.fetch(lMessage.id);
+			return await interaction.reply({
+				embeds: [generateEmbed(fetchedMessage)],
+			});
+		}
+		catch (e) {
+			return await interaction.reply({ content: 'A mensagem enviada por este usuário não existe mais.', ephemeral: true });
+		}
 	},
 };
 
-function generateEmbed(data: LastMessage): MessageEmbed {
-	return new MessageEmbed().setDescription(data.id as string);
+function generateEmbed(message: Message): MessageEmbed {
+	const date = new Date(message.createdAt);
+	const string = date.toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+
+	return new MessageEmbed()
+		.setTitle(`Última mensagem do(a) ${message.author.tag}`)
+		.addField('Conteúdo', message.content)
+		.addField('Data', string)
+		.setColor('RANDOM')
+		.setThumbnail(message.author.avatarURL() ?? 'https://discord.com/assets/9f6f9cd156ce35e2d94c0e62e3eff462.png');
 }
